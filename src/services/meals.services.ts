9aa0cbbd8c9ca097226695cd1cdc2860d220e3c6
Eman Radwan <emanalaa11499@gmail.com>
2026-02-25 07:45:42 +0200

    fix edge functions config

--- src/services/meals.services.ts
+++ src/services/meals.services.ts
@@ -1,9 +1,15 @@
-import { supabase } from "@/utils/supabase";
+import { supabase, supabaseKey } from "@/utils/supabase";
 
 // get meals
 export const fetchMealData = async (searchTerm: string) => {
   const response = await fetch(
     `https://aulmhefinbmppfiouebn.supabase.co/functions/v1/searchMeals?searchTerm=${encodeURIComponent(searchTerm)}`,
+    {
+      headers: {
+        apikey: supabaseKey,
+        Authorization: `Bearer ${supabaseKey}`,
+      },
+    },
   );
   const meals = await response.json();
   return meals;
